433396bfab5074ec286641926db5e0f14d7111f1
yudt <ydt210281@163.com>
2018-01-19 11:19:18 +0800

    完成UI页面

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="pre-order-info-bg">
-    <div>
-      <el-button type="primary" size="small" @click="check">查看占用情况</el-button>
-      <div v-if="check_tapped">
-        <div>
-          <span>会议室占用情况:</span>
-          <div>
-            <div></div>
-            <span>{{status_str}}</span>
+    <div class="content">
+      <div class="top">
+        <div class="top-left">
+          <el-button type="primary" size="small" @click="check">查看占用情况</el-button>
+          <el-button type="primary" :disabled="!can_pre_order" size="small" @click="">预定</el-button>
+        </div>
+        <div class="top-right">
+          <div class="pre-order-info">
+            <div class="status">
+              <span>会议室占用情况:</span>
+              <div>
+                <div :class="{busy:1 == pre_order_info.status,unknown:0 == pre_order_info.status,avail:2 == pre_order_info.status}"></div>
+                <span>{{status_str}}</span>
+              </div>
+            </div>
+            <div class="left_time">
+              <span>剩余时间:</span>
+              <span>{{pre_order_info.left_time}}</span>
+            </div>
+            <div class="holder">
+              <span>占用者:</span>
+              <span>{{pre_order_info.holder}}</span>
+            </div>
           </div>
         </div>
-        <div>
-          <span>剩余时间:</span>
-          <span>{{pre_order_info.left_time}}</span>
-        </div>
-        <div>
-          <span>占用者:</span>
-          <span>{{pre_order_info.holder}}</span>
-        </div>
       </div>
-    </div>
-    <div>
-      <el-button type="primary" :disabled="!can_pre_order" size="small" @click="">预定</el-button>
     </div>
   </div>
 </template>
@@ -34,8 +38,7 @@
           status: 0,
           holder: '',
           left_time: 0
-        },
-        check_tapped: false
+        }
       }
     },
     computed:{
@@ -57,10 +60,12 @@
     },
     methods:{
       check(){
-        this.check_tapped = true;
+
       },
       pre_order(){
-        
+        if(this.can_pre_order){
+
+        }
       }
     }
   }
@@ -68,49 +73,106 @@
 <style lang="scss" scoped>
   #pre-order-info-bg{
     display: flex;
-    flex-direction: column;
     align-items: center;
-    div:first-of-type{
+    justify-content: center;
+    height: 100vh;
+    .content{
       display: flex;
-      &>div{
-        margin-left: 32px;
-        width: 200px;
-        height: 300px;
-        border: 1px dashed navy;
-        border-radius: 8px;
-        &>div{
+      flex-direction: column;
+      align-items: center;
+      background-color: cornsilk;
+      width: 80%;
+      height: 80%;
+      border: 3px solid navy;
+      border-radius: 4px;
+      .top{
+        display: flex;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        padding: 32px;
+        box-sizing: border-box;
+        width: 100%;
+        .top-left{
+          width: 50%;
+          height: 100%;
           display: flex;
-        }
-        &>div:first-of-type{
-          &>div{
-            &>span{
-              margin-right: 16px;
-            }
-            &>div{
-              width: 80px;
-              height: 80px;
-            }
-            .busy{
-              background-color: red;
-            }
-            .avail{
-              background-color: lime;
-            }
-            .unknown{
-              background-color: aliceblue;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          &>.el-button{
+            width: 160px;
+            padding: 0;
+            height: 32px;
+            border-radius: 16px;
+            margin-left: 0;
+            &:last-of-type{
+              margin-top: 64px;
             }
           }
         }
-        &>div:nth-of-type(2){
-          margin-top: 16px;
-        }
-        &>div:last-of-type{
-          margin-top: 16px;
+        .top-right{
+          width: 50%;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          &>.pre-order-info{
+            width: 100%;
+            height: 100%;
+            border: 2px dashed dodgerblue;
+            border-radius: 8px;
+            &>div{
+              display: flex;
+              align-items: center;
+              height: 33.33%;
+              &>*{
+                margin-left: 32px;
+              }
+            }
+            &>.status{
+              &>div{
+                display: flex;
+                align-items: center;
+                &>span{
+                  margin-left: 16px;
+                }
+                &>div{
+                  width: 80px;
+                  height: 80px;
+                }
+                .busy{
+                  background-color: red;
+                }
+                .avail{
+                  background-color: lime;
+                }
+                .unknown{
+                  background-color: dimgrey;
+                }
+              }
+            }
+            &>.left-time{
+              margin-top: 16px;
+            }
+            &>.holder{
+              margin-top: 16px;
+            }
+          }
         }
       }
-    }
-    div:last-of-type{
-      margin-top: 56px;
+      .bottom{
+        width: 100%;
+        height: 20%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .el-button{
+          width: 160px;
+          padding: 0;
+          height: 32px;
+          border-radius: 16px;
+        }
+      }
     }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
